c2dc593d03709d4794989b90f1f13ad54e577c2f
Amit <amit.maurya@adjecti.com>
2022-11-10 12:03:34 +0530

    new module added in jetprocess

---
+++ modules/jet-process/jet-process-web/src/main/java/io/jetprocess/web/constants/JetProcessWebPortletKeys.java
@@ -0,0 +1,11 @@
+package io.jetprocess.web.constants;
+
+/**
+ * @author Admin
+ */
+public class JetProcessWebPortletKeys {
+
+	public static final String JETPROCESSWEB =
+		"io_jetprocess_web_JetProcessWebPortlet";
+
+}
---
+++ modules/jet-process/jet-process-web/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="jetprocessweb.caption"/></b>
+</p>
