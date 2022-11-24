332b10fe5e3db4e8df5250ba96370eb92e754f9d
Urvashi <uravshi.singh@adjecti.com>
2022-11-24 16:57:17 +0530

    common.js

--- modules/jet-process/jet-process-web/src/main/resources/META-INF/resources/js/common.js
+++ modules/jet-process/jet-process-web/src/main/resources/META-INF/resources/js/common.js
@@ -1,20 +1,21 @@
 <script >
    function bindFormDataJson(formObj){
+	 //  var list= $('#<portlet:namespace/>formName')[0];
 	   var formdata = new FormData(formObj);
 	   var portletnamespace = '<portlet:namespace/>';
-	   console.log(portletnamespace);
 	   var object = {};
   	 formdata.forEach(function(value, key){
   	  var keySpace = key.trim();
-      console.log(keySpace);
+  
   	  var splitnamespace = keySpace.split(portletnamespace)[1];
-  	  console.log(splitnamespace);
   	 var test = splitnamespace;
+  	
+  	 
 	   if (test=="formDate" || test == ""){
 		   delete object[test] ;   
 	   }else{
 		 object[test] = value;
-		 console.log(object);
+		 console.log("object"+object);
 		
 	   } 	  
 	 });
