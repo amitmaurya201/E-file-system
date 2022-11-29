ba1106fe27fcce703ace3001978961ec8909396b
unknown <aparanasingh169@gmail.com>
2022-11-29 21:31:14 +0530

    save method on portlet for filemovement and receiptmovement

--- modules/jet-process/jet-process-web/src/main/java/io/jetprocess/web/portlet/JetProcessWebPortlet.java
+++ modules/jet-process/jet-process-web/src/main/java/io/jetprocess/web/portlet/JetProcessWebPortlet.java
@@ -1,30 +1,74 @@
 package io.jetprocess.web.portlet;
 
+import io.jetprocess.service.FileMovementLocalService;
+import io.jetprocess.service.ReceiptMovementLocalService;
 import io.jetprocess.web.constants.JetProcessWebPortletKeys;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.util.ParamUtil;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.Portlet;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Admin
  */
-@Component(
-	immediate = true,
-	property = {
-		"com.liferay.portlet.display-category=category.sample",
-		"com.liferay.portlet.header-portlet-css=/css/main.css",
-		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=JetProcessWeb",
-		"javax.portlet.init-param.template-path=/",
+@Component(immediate = true, property = { "com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css", "com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=JetProcessWeb", "javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + JetProcessWebPortletKeys.JETPROCESSWEB,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
-	},
-	service = Portlet.class
-)
+		"javax.portlet.security-role-ref=power-user,user" }, service = Portlet.class)
 public class JetProcessWebPortlet extends MVCPortlet {
+
+	public void sendFile(ActionRequest actionRequest, ActionResponse actionResponse) {
+		long receiverId = ParamUtil.get(actionRequest, "receiverId", 0);
+		long senderId = ParamUtil.get(actionRequest, "senderId", 0);
+		long fileId = ParamUtil.get(actionRequest, "fileId", 0);
+		String remark = ParamUtil.getString(actionRequest, "remark");
+		String dueDate = ParamUtil.getString(actionRequest, "dueDate");
+		String priority = ParamUtil.getString(actionRequest, "priorty");
+		// fLocalService.saveSendFile(receiverId, senderId, fileId, priority, dueDate,
+		// remark);
+
+		// UserPostServiceUtil.getUserPostSearchedData(data);
+
+		System.out.println("receiverId-- > " + receiverId);
+		System.out.println("senderId-- > " + senderId);
+		System.out.println("fileId-- > " + fileId);
+		System.out.println("remark-- > " + remark);
+		System.out.println("dueDate-- > " + dueDate);
+		System.out.println("priority-> " + priority);
+
+	}
+
+	public void sendReceipt(ActionRequest actionRequest, ActionResponse actionResponse) {
+		long receiverId = ParamUtil.get(actionRequest, "receiverId", 0);
+		long senderId = ParamUtil.get(actionRequest, "senderId", 0);
+		long receiptId = ParamUtil.get(actionRequest, "receiptId", 0);
+		String remark = ParamUtil.getString(actionRequest, "remark");
+		String dueDate = ParamUtil.getString(actionRequest, "dueDate");
+		String priority = ParamUtil.getString(actionRequest, "priorty");
+		// receiptMovementLocalService.saveSendReceipt(receiverId, senderId, receiptId,
+		// priority, dueDate, remark);
+
+		System.out.println("receiverId-- > " + receiverId);
+		System.out.println("senderId-- > " + senderId);
+		System.out.println("receiptId-- > " + receiptId);
+		System.out.println("remark-- > " + remark);
+		System.out.println("dueDate-- > " + dueDate);
+		System.out.println("priority-> " + priority);
+
+	}
+
+	@Reference
+	FileMovementLocalService fLocalService;
+
+	@Reference
+	ReceiptMovementLocalService receiptMovementLocalService;
 }
