50b6884a1c84193607854a16b37f3ae3560faae0
Aparana Singh <aparanasingh169@gmail.com>
2022-12-01 12:35:22 +0530

    fileMovement and ReceiptMovement service class

---
+++ modules/jet-process/jet-process-api/src/main/java/io/jetprocess/model/ReceiptMovementWrapper.java
@@ -0,0 +1,464 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package io.jetprocess.model;
+
+import com.liferay.exportimport.kernel.lar.StagedModelType;
+import com.liferay.portal.kernel.model.ModelWrapper;
+import com.liferay.portal.kernel.model.wrapper.BaseModelWrapper;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>
+ * This class is a wrapper for {@link ReceiptMovement}.
+ * </p>
+ *
+ * @author Brian Wing Shun Chan
+ * @see ReceiptMovement
+ * @generated
+ */
+public class ReceiptMovementWrapper
+	extends BaseModelWrapper<ReceiptMovement>
+	implements ModelWrapper<ReceiptMovement>, ReceiptMovement {
+
+	public ReceiptMovementWrapper(ReceiptMovement receiptMovement) {
+		super(receiptMovement);
+	}
+
+	@Override
+	public Map<String, Object> getModelAttributes() {
+		Map<String, Object> attributes = new HashMap<String, Object>();
+
+		attributes.put("uuid", getUuid());
+		attributes.put("rmId", getRmId());
+		attributes.put("groupId", getGroupId());
+		attributes.put("companyId", getCompanyId());
+		attributes.put("userId", getUserId());
+		attributes.put("createDate", getCreateDate());
+		attributes.put("modifiedDate", getModifiedDate());
+		attributes.put("receiverId", getReceiverId());
+		attributes.put("senderId", getSenderId());
+		attributes.put("receiptId", getReceiptId());
+		attributes.put("priority", getPriority());
+		attributes.put("dueDate", getDueDate());
+		attributes.put("remark", getRemark());
+
+		return attributes;
+	}
+
+	@Override
+	public void setModelAttributes(Map<String, Object> attributes) {
+		String uuid = (String)attributes.get("uuid");
+
+		if (uuid != null) {
+			setUuid(uuid);
+		}
+
+		Long rmId = (Long)attributes.get("rmId");
+
+		if (rmId != null) {
+			setRmId(rmId);
+		}
+
+		Long groupId = (Long)attributes.get("groupId");
+
+		if (groupId != null) {
+			setGroupId(groupId);
+		}
+
+		Long companyId = (Long)attributes.get("companyId");
+
+		if (companyId != null) {
+			setCompanyId(companyId);
+		}
+
+		Long userId = (Long)attributes.get("userId");
+
+		if (userId != null) {
+			setUserId(userId);
+		}
+
+		Date createDate = (Date)attributes.get("createDate");
+
+		if (createDate != null) {
+			setCreateDate(createDate);
+		}
+
+		Date modifiedDate = (Date)attributes.get("modifiedDate");
+
+		if (modifiedDate != null) {
+			setModifiedDate(modifiedDate);
+		}
+
+		Long receiverId = (Long)attributes.get("receiverId");
+
+		if (receiverId != null) {
+			setReceiverId(receiverId);
+		}
+
+		Long senderId = (Long)attributes.get("senderId");
+
+		if (senderId != null) {
+			setSenderId(senderId);
+		}
+
+		Long receiptId = (Long)attributes.get("receiptId");
+
+		if (receiptId != null) {
+			setReceiptId(receiptId);
+		}
+
+		String priority = (String)attributes.get("priority");
+
+		if (priority != null) {
+			setPriority(priority);
+		}
+
+		String dueDate = (String)attributes.get("dueDate");
+
+		if (dueDate != null) {
+			setDueDate(dueDate);
+		}
+
+		String remark = (String)attributes.get("remark");
+
+		if (remark != null) {
+			setRemark(remark);
+		}
+	}
+
+	@Override
+	public ReceiptMovement cloneWithOriginalValues() {
+		return wrap(model.cloneWithOriginalValues());
+	}
+
+	/**
+	 * Returns the company ID of this receipt movement.
+	 *
+	 * @return the company ID of this receipt movement
+	 */
+	@Override
+	public long getCompanyId() {
+		return model.getCompanyId();
+	}
+
+	/**
+	 * Returns the create date of this receipt movement.
+	 *
+	 * @return the create date of this receipt movement
+	 */
+	@Override
+	public Date getCreateDate() {
+		return model.getCreateDate();
+	}
+
+	/**
+	 * Returns the due date of this receipt movement.
+	 *
+	 * @return the due date of this receipt movement
+	 */
+	@Override
+	public String getDueDate() {
+		return model.getDueDate();
+	}
+
+	/**
+	 * Returns the group ID of this receipt movement.
+	 *
+	 * @return the group ID of this receipt movement
+	 */
+	@Override
+	public long getGroupId() {
+		return model.getGroupId();
+	}
+
+	/**
+	 * Returns the modified date of this receipt movement.
+	 *
+	 * @return the modified date of this receipt movement
+	 */
+	@Override
+	public Date getModifiedDate() {
+		return model.getModifiedDate();
+	}
+
+	/**
+	 * Returns the primary key of this receipt movement.
+	 *
+	 * @return the primary key of this receipt movement
+	 */
+	@Override
+	public long getPrimaryKey() {
+		return model.getPrimaryKey();
+	}
+
+	/**
+	 * Returns the priority of this receipt movement.
+	 *
+	 * @return the priority of this receipt movement
+	 */
+	@Override
+	public String getPriority() {
+		return model.getPriority();
+	}
+
+	/**
+	 * Returns the receipt ID of this receipt movement.
+	 *
+	 * @return the receipt ID of this receipt movement
+	 */
+	@Override
+	public long getReceiptId() {
+		return model.getReceiptId();
+	}
+
+	/**
+	 * Returns the receiver ID of this receipt movement.
+	 *
+	 * @return the receiver ID of this receipt movement
+	 */
+	@Override
+	public long getReceiverId() {
+		return model.getReceiverId();
+	}
+
+	/**
+	 * Returns the remark of this receipt movement.
+	 *
+	 * @return the remark of this receipt movement
+	 */
+	@Override
+	public String getRemark() {
+		return model.getRemark();
+	}
+
+	/**
+	 * Returns the rm ID of this receipt movement.
+	 *
+	 * @return the rm ID of this receipt movement
+	 */
+	@Override
+	public long getRmId() {
+		return model.getRmId();
+	}
+
+	/**
+	 * Returns the sender ID of this receipt movement.
+	 *
+	 * @return the sender ID of this receipt movement
+	 */
+	@Override
+	public long getSenderId() {
+		return model.getSenderId();
+	}
+
+	/**
+	 * Returns the user ID of this receipt movement.
+	 *
+	 * @return the user ID of this receipt movement
+	 */
+	@Override
+	public long getUserId() {
+		return model.getUserId();
+	}
+
+	/**
+	 * Returns the user uuid of this receipt movement.
+	 *
+	 * @return the user uuid of this receipt movement
+	 */
+	@Override
+	public String getUserUuid() {
+		return model.getUserUuid();
+	}
+
+	/**
+	 * Returns the uuid of this receipt movement.
+	 *
+	 * @return the uuid of this receipt movement
+	 */
+	@Override
+	public String getUuid() {
+		return model.getUuid();
+	}
+
+	@Override
+	public void persist() {
+		model.persist();
+	}
+
+	/**
+	 * Sets the company ID of this receipt movement.
+	 *
+	 * @param companyId the company ID of this receipt movement
+	 */
+	@Override
+	public void setCompanyId(long companyId) {
+		model.setCompanyId(companyId);
+	}
+
+	/**
+	 * Sets the create date of this receipt movement.
+	 *
+	 * @param createDate the create date of this receipt movement
+	 */
+	@Override
+	public void setCreateDate(Date createDate) {
+		model.setCreateDate(createDate);
+	}
+
+	/**
+	 * Sets the due date of this receipt movement.
+	 *
+	 * @param dueDate the due date of this receipt movement
+	 */
+	@Override
+	public void setDueDate(String dueDate) {
+		model.setDueDate(dueDate);
+	}
+
+	/**
+	 * Sets the group ID of this receipt movement.
+	 *
+	 * @param groupId the group ID of this receipt movement
+	 */
+	@Override
+	public void setGroupId(long groupId) {
+		model.setGroupId(groupId);
+	}
+
+	/**
+	 * Sets the modified date of this receipt movement.
+	 *
+	 * @param modifiedDate the modified date of this receipt movement
+	 */
+	@Override
+	public void setModifiedDate(Date modifiedDate) {
+		model.setModifiedDate(modifiedDate);
+	}
+
+	/**
+	 * Sets the primary key of this receipt movement.
+	 *
+	 * @param primaryKey the primary key of this receipt movement
+	 */
+	@Override
+	public void setPrimaryKey(long primaryKey) {
+		model.setPrimaryKey(primaryKey);
+	}
+
+	/**
+	 * Sets the priority of this receipt movement.
+	 *
+	 * @param priority the priority of this receipt movement
+	 */
+	@Override
+	public void setPriority(String priority) {
+		model.setPriority(priority);
+	}
+
+	/**
+	 * Sets the receipt ID of this receipt movement.
+	 *
+	 * @param receiptId the receipt ID of this receipt movement
+	 */
+	@Override
+	public void setReceiptId(long receiptId) {
+		model.setReceiptId(receiptId);
+	}
+
+	/**
+	 * Sets the receiver ID of this receipt movement.
+	 *
+	 * @param receiverId the receiver ID of this receipt movement
+	 */
+	@Override
+	public void setReceiverId(long receiverId) {
+		model.setReceiverId(receiverId);
+	}
+
+	/**
+	 * Sets the remark of this receipt movement.
+	 *
+	 * @param remark the remark of this receipt movement
+	 */
+	@Override
+	public void setRemark(String remark) {
+		model.setRemark(remark);
+	}
+
+	/**
+	 * Sets the rm ID of this receipt movement.
+	 *
+	 * @param rmId the rm ID of this receipt movement
+	 */
+	@Override
+	public void setRmId(long rmId) {
+		model.setRmId(rmId);
+	}
+
+	/**
+	 * Sets the sender ID of this receipt movement.
+	 *
+	 * @param senderId the sender ID of this receipt movement
+	 */
+	@Override
+	public void setSenderId(long senderId) {
+		model.setSenderId(senderId);
+	}
+
+	/**
+	 * Sets the user ID of this receipt movement.
+	 *
+	 * @param userId the user ID of this receipt movement
+	 */
+	@Override
+	public void setUserId(long userId) {
+		model.setUserId(userId);
+	}
+
+	/**
+	 * Sets the user uuid of this receipt movement.
+	 *
+	 * @param userUuid the user uuid of this receipt movement
+	 */
+	@Override
+	public void setUserUuid(String userUuid) {
+		model.setUserUuid(userUuid);
+	}
+
+	/**
+	 * Sets the uuid of this receipt movement.
+	 *
+	 * @param uuid the uuid of this receipt movement
+	 */
+	@Override
+	public void setUuid(String uuid) {
+		model.setUuid(uuid);
+	}
+
+	@Override
+	public StagedModelType getStagedModelType() {
+		return model.getStagedModelType();
+	}
+
+	@Override
+	protected ReceiptMovementWrapper wrap(ReceiptMovement receiptMovement) {
+		return new ReceiptMovementWrapper(receiptMovement);
+	}
+
+}
---
+++ modules/jet-process/jet-process-service/src/main/java/io/jetprocess/model/impl/ReceiptMovementCacheModel.java
@@ -0,0 +1,244 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package io.jetprocess.model.impl;
+
+import com.liferay.petra.lang.HashUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.model.CacheModel;
+
+import io.jetprocess.model.ReceiptMovement;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+import java.util.Date;
+
+/**
+ * The cache model class for representing ReceiptMovement in entity cache.
+ *
+ * @author Brian Wing Shun Chan
+ * @generated
+ */
+public class ReceiptMovementCacheModel
+	implements CacheModel<ReceiptMovement>, Externalizable {
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+
+		if (!(object instanceof ReceiptMovementCacheModel)) {
+			return false;
+		}
+
+		ReceiptMovementCacheModel receiptMovementCacheModel =
+			(ReceiptMovementCacheModel)object;
+
+		if (rmId == receiptMovementCacheModel.rmId) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return HashUtil.hash(0, rmId);
+	}
+
+	@Override
+	public String toString() {
+		StringBundler sb = new StringBundler(27);
+
+		sb.append("{uuid=");
+		sb.append(uuid);
+		sb.append(", rmId=");
+		sb.append(rmId);
+		sb.append(", groupId=");
+		sb.append(groupId);
+		sb.append(", companyId=");
+		sb.append(companyId);
+		sb.append(", userId=");
+		sb.append(userId);
+		sb.append(", createDate=");
+		sb.append(createDate);
+		sb.append(", modifiedDate=");
+		sb.append(modifiedDate);
+		sb.append(", receiverId=");
+		sb.append(receiverId);
+		sb.append(", senderId=");
+		sb.append(senderId);
+		sb.append(", receiptId=");
+		sb.append(receiptId);
+		sb.append(", priority=");
+		sb.append(priority);
+		sb.append(", dueDate=");
+		sb.append(dueDate);
+		sb.append(", remark=");
+		sb.append(remark);
+		sb.append("}");
+
+		return sb.toString();
+	}
+
+	@Override
+	public ReceiptMovement toEntityModel() {
+		ReceiptMovementImpl receiptMovementImpl = new ReceiptMovementImpl();
+
+		if (uuid == null) {
+			receiptMovementImpl.setUuid("");
+		}
+		else {
+			receiptMovementImpl.setUuid(uuid);
+		}
+
+		receiptMovementImpl.setRmId(rmId);
+		receiptMovementImpl.setGroupId(groupId);
+		receiptMovementImpl.setCompanyId(companyId);
+		receiptMovementImpl.setUserId(userId);
+
+		if (createDate == Long.MIN_VALUE) {
+			receiptMovementImpl.setCreateDate(null);
+		}
+		else {
+			receiptMovementImpl.setCreateDate(new Date(createDate));
+		}
+
+		if (modifiedDate == Long.MIN_VALUE) {
+			receiptMovementImpl.setModifiedDate(null);
+		}
+		else {
+			receiptMovementImpl.setModifiedDate(new Date(modifiedDate));
+		}
+
+		receiptMovementImpl.setReceiverId(receiverId);
+		receiptMovementImpl.setSenderId(senderId);
+		receiptMovementImpl.setReceiptId(receiptId);
+
+		if (priority == null) {
+			receiptMovementImpl.setPriority("");
+		}
+		else {
+			receiptMovementImpl.setPriority(priority);
+		}
+
+		if (dueDate == null) {
+			receiptMovementImpl.setDueDate("");
+		}
+		else {
+			receiptMovementImpl.setDueDate(dueDate);
+		}
+
+		if (remark == null) {
+			receiptMovementImpl.setRemark("");
+		}
+		else {
+			receiptMovementImpl.setRemark(remark);
+		}
+
+		receiptMovementImpl.resetOriginalValues();
+
+		return receiptMovementImpl;
+	}
+
+	@Override
+	public void readExternal(ObjectInput objectInput) throws IOException {
+		uuid = objectInput.readUTF();
+
+		rmId = objectInput.readLong();
+
+		groupId = objectInput.readLong();
+
+		companyId = objectInput.readLong();
+
+		userId = objectInput.readLong();
+		createDate = objectInput.readLong();
+		modifiedDate = objectInput.readLong();
+
+		receiverId = objectInput.readLong();
+
+		senderId = objectInput.readLong();
+
+		receiptId = objectInput.readLong();
+		priority = objectInput.readUTF();
+		dueDate = objectInput.readUTF();
+		remark = objectInput.readUTF();
+	}
+
+	@Override
+	public void writeExternal(ObjectOutput objectOutput) throws IOException {
+		if (uuid == null) {
+			objectOutput.writeUTF("");
+		}
+		else {
+			objectOutput.writeUTF(uuid);
+		}
+
+		objectOutput.writeLong(rmId);
+
+		objectOutput.writeLong(groupId);
+
+		objectOutput.writeLong(companyId);
+
+		objectOutput.writeLong(userId);
+		objectOutput.writeLong(createDate);
+		objectOutput.writeLong(modifiedDate);
+
+		objectOutput.writeLong(receiverId);
+
+		objectOutput.writeLong(senderId);
+
+		objectOutput.writeLong(receiptId);
+
+		if (priority == null) {
+			objectOutput.writeUTF("");
+		}
+		else {
+			objectOutput.writeUTF(priority);
+		}
+
+		if (dueDate == null) {
+			objectOutput.writeUTF("");
+		}
+		else {
+			objectOutput.writeUTF(dueDate);
+		}
+
+		if (remark == null) {
+			objectOutput.writeUTF("");
+		}
+		else {
+			objectOutput.writeUTF(remark);
+		}
+	}
+
+	public String uuid;
+	public long rmId;
+	public long groupId;
+	public long companyId;
+	public long userId;
+	public long createDate;
+	public long modifiedDate;
+	public long receiverId;
+	public long senderId;
+	public long receiptId;
+	public String priority;
+	public String dueDate;
+	public String remark;
+
+}
