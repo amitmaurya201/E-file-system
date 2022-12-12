e2c89b8462c75a8b85af13f5535836cee18103b8
Aparana Singh <aparanasingh169@gmail.com>
2022-12-12 17:57:31 +0530

    create viewsend file render command

---
+++ modules/jet-process/jet-process-web/src/main/java/io/jetprocess/web/render/viewSendFileRenderCommand.java
@@ -0,0 +1,53 @@
+package io.jetprocess.web.render;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import io.jetprocess.model.DocFile;
+import io.jetprocess.service.DocFileLocalServiceUtil;
+import io.jetprocess.web.constants.JetProcessWebPortletKeys;
+import io.jetprocess.web.constants.MVCCommandNames;
+
+@Component(
+		immediate = true,
+		property = {
+			"javax.portlet.name=" + JetProcessWebPortletKeys.JETPROCESSWEB,
+			"mvc.command.name=" + MVCCommandNames.FILE_SEND_RENDER_COMMAND
+			
+		},
+		service = MVCRenderCommand.class
+	)
+
+public class viewSendFileRenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(RenderRequest renderRequest, RenderResponse renderResponse) throws PortletException {
+		long docFileId = ParamUtil.getLong(renderRequest, "docFileId");
+		logger.info("docFileId---> "+docFileId);
+		
+		try {
+			DocFile docFile=DocFileLocalServiceUtil.getDocFile(docFileId);
+			logger.info("-docFile-> : "+docFile);
+			renderRequest.setAttribute("docFile", docFile);
+		} catch (PortalException e) {
+			e.printStackTrace();
+		}
+		return "/file/send.jsp";
+	}
+	
+	private Log logger = LogFactoryUtil.getLog(this.getClass());
+	
+	
+	 
+
+}
