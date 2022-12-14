0a4c9620b496d4b42af19bb7ac396cf5cfe2dbdf
Amit <amit.maurya@adjecti.com>
2022-12-14 18:07:46 +0530

    fix

--- modules/jet-process/jet-process-web/src/main/java/io/jetprocess/web/action/command/FileInboxReceiveActionCommand.java
+++ modules/jet-process/jet-process-web/src/main/java/io/jetprocess/web/action/command/FileInboxReceiveActionCommand.java
@@ -1,42 +1,47 @@
 package io.jetprocess.web.action.command;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.PortletException;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
+import io.jetprocess.model.FileMovement;
+import io.jetprocess.service.FileMovementLocalService;
 import io.jetprocess.web.constants.JetProcessWebPortletKeys;
 
-@Component(
-		immediate = true,
-		property = {
-				"javax.portlet.name=" + JetProcessWebPortletKeys.JETPROCESSWEB,
-			"mvc.command.name=inbox"
-		},
-		service = MVCActionCommand.class
-	)
+@Component(immediate = true, property = { "javax.portlet.name=" + JetProcessWebPortletKeys.JETPROCESSWEB,
+		"mvc.command.name=receiveAction" }, service = MVCActionCommand.class)
 
-public class FileInboxActionCommand  implements MVCActionCommand{
+public class FileInboxReceiveActionCommand implements MVCActionCommand {
+
+	
+	@Reference
+	private FileMovementLocalService fileMovementLocalService;
 
 	@Override
 	public boolean processAction(ActionRequest actionRequest, ActionResponse actionResponse) throws PortletException {
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		return false;
-	}
+		long fileId = ParamUtil.getLong(actionRequest, "fileId");
 
+		System.out.println("fileId1 --:" + fileId);
+
+		FileMovement fileMovement = fileMovementLocalService.getFileByFileId(fileId);
+	
+		if (fileMovement.getFileId() == fileId) {
+			fileMovement.setReceivedOn("receive");
+			fileMovementLocalService.updateFileMovement(fileMovement);
+		}
+		
+		actionResponse.setRenderParameter("mvcPath", "/file/inbox.jsp");
+
+		return false;
+		
+		
+
+	}
 }
