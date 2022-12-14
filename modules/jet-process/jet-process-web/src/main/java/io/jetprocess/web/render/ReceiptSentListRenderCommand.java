9d6a0c5f485b1897b39d8b96a11e862501311a43
Urvashi <urvashi.singh@adjecti.com>
2022-12-14 15:30:45 +0530

    fix the receipt sent list rendercommandname class

--- modules/jet-process/jet-process-web/src/main/java/io/jetprocess/web/render/ReceiptSentListRenderCommand.java
+++ modules/jet-process/jet-process-web/src/main/java/io/jetprocess/web/render/ReceiptSentListRenderCommand.java
@@ -12,11 +12,12 @@ import io.jetprocess.web.constants.JetProcessWebPortletKeys;
 import io.jetprocess.web.constants.MVCCommandNames;
 
 @Component(immediate = true, property = { "javax.portlet.name=" + JetProcessWebPortletKeys.JETPROCESSWEB,
-		"mvc.command.name="+MVCCommandNames.RECEIPT_SEND_RENDER_COMMAND }, service = MVCRenderCommand.class)
+		"mvc.command.name=" + MVCCommandNames.RECEIPT_SENT_LIST }, service = MVCRenderCommand.class)
 public class ReceiptSentListRenderCommand implements MVCRenderCommand {
 
 	@Override
 	public String render(RenderRequest renderRequest, RenderResponse renderResponse) throws PortletException {
+		System.out.println("receipt sent render command");
 		return "/receipt/sent_list.jsp";
 	}
 
