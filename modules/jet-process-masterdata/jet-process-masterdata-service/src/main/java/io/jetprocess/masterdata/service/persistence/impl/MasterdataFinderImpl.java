35eb220147b99873ad7b521355c682908f020805
Lohit Sagar Mishra <lohit.mishra@adjecti.com>
2022-12-22 15:38:41 +0530

    fix

--- modules/jet-process-masterdata/jet-process-masterdata-service/src/main/java/io/jetprocess/masterdata/service/persistence/impl/MasterdataFinderImpl.java
+++ modules/jet-process-masterdata/jet-process-masterdata-service/src/main/java/io/jetprocess/masterdata/service/persistence/impl/MasterdataFinderImpl.java
@@ -1056,7 +1056,7 @@ public class MasterdataFinderImpl extends MasterdataFinderBaseImpl implements Ma
 	
 			String sql = "SELECT receiptid as receiptId , receiptnumber as receiptnumber , subject as subject , categoryvalue as category , createDate as createDate ,  remarks as remark , viewpdfurl, nature " + 
 					"			FROM jet_process_receipt INNER JOIN" + 
-					"			md_category  ON categorydataid = receiptcategoryid where userpostid = ? AND  currentstate = 1 AND attachstatus IS NULL";
+					"			md_category  ON categorydataid = receiptcategoryid where userpostid = ? AND  currentstate = 1 AND attachstatus IS NULL ";
 			
 		
 			if(!keyword.isEmpty() && keyword != null ) {
@@ -1157,7 +1157,7 @@ public class MasterdataFinderImpl extends MasterdataFinderBaseImpl implements Ma
 			session = openSession();
 			String sql = "SELECT receiptid as receiptId , receiptnumber as receiptnumber , subject as subject , categoryvalue as category , createDate as createDate ,  remarks as remark , viewpdfurl, nature " + 
 					"	nature FROM jet_process_receipt INNER JOIN " + 
-					"	md_category  ON categorydataid = receiptcategoryid where userpostid = ? AND currentstate = 1 AND attachstatus IS NULL";
+					"	md_category  ON categorydataid = receiptcategoryid where userpostid = ? AND currentstate = 1 AND attachstatus IS NULL ";
 			if(!keyword.isEmpty() && keyword != null ) {
 				sql = sql+"AND (receiptnumber ilike ? OR subject ilike ? OR  categoryvalue ilike ?)";
 			}
