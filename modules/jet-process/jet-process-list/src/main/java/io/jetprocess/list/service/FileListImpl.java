033227a21b2d912e50c244aeb5c3020a4c268b35
Amit <amit.maurya@adjecti.com>
2022-12-29 18:50:08 +0530

    list functions by ashwani

--- modules/jet-process/jet-process-list/src/main/java/io/jetprocess/list/service/FileListImpl.java
+++ modules/jet-process/jet-process-list/src/main/java/io/jetprocess/list/service/FileListImpl.java
@@ -15,193 +15,246 @@ import io.jetprocess.list.api.FileList;
 import io.jetprocess.masterdata.model.FileListViewDto;
 import io.jetprocess.masterdata.model.FileMovementDTO;
 
-@Component(immediate = true, service=FileList.class)
-public class FileListImpl implements FileList{
+@Component(immediate = true, service = FileList.class)
+public class FileListImpl implements FileList {
 
+	public int getCountOfFileList(long postId, String keyword) {
 
-public  int getCountOfFileList(long postId, String keyword) {
-		
-		Connection con=null;
-		int count=0;
-		 try {
+		Connection con = null;
+		int count = 0;
+		try {
 			con = DataAccess.getConnection();
 			CallableStatement prepareCall = con.prepareCall("select file_count_by_keyword(?,?)");
 			prepareCall.setLong(1, postId);
 			prepareCall.setString(2, keyword);
 			boolean execute = prepareCall.execute();
-			
-			if(execute) {
-				ResultSet rs=prepareCall.getResultSet();
+
+			if (execute) {
+				ResultSet rs = prepareCall.getResultSet();
 				if (rs.next()) {
-				    count= rs.getInt(1);
+					count = rs.getInt(1);
 				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			DataAccess.cleanUp(con);
 		}
-		 finally {
-			 DataAccess.cleanUp(con);
-		 }
-		 
-		 
-		
+
 		return count;
 	}
 
+	public List<FileListViewDto> getFileList(long userPostId, String keyword, int start, int end, String orderBy,
+			String order) {
 
-public List<FileListViewDto> getFileList(long userPostId, String keyword, int start, int end,
-		String orderBy, String order){
-	
-	
-	List<FileListViewDto> fileList=new ArrayList<>();
-	
-	Connection con=null;
-	
-	 try {
-		con = DataAccess.getConnection();
-		CallableStatement prepareCall = con.prepareCall("SELECT * from public.get_file_list(?,?,?,?,?,?)");
-		prepareCall.setLong(1, userPostId);
-		prepareCall.setString(2, keyword);
-		prepareCall.setInt(3, start);
-		prepareCall.setInt(4, end);
-		prepareCall.setString(5, orderBy);
-		prepareCall.setString(6, order);
-		boolean execute = prepareCall.execute();
-		
-		if(execute) {
-			ResultSet rs=prepareCall.getResultSet();
-			while (rs.next()) {
-				FileListViewDto file=new FileListViewDto();
-				
-				System.out.println("category : "+rs.getString("category")+
-						", created on : "+rs.getDate("createdon")+", doc File : "+
-						rs.getLong("docfileid")+", File number : "+rs.getString("filenumber")
-								
-								);
-			    file.setCategory(rs.getString("category"));
-			    file.setCreateDate(rs.getDate("createdon"));
-			    file.setDocFileId(rs.getLong("docfileid"));
-			    file.setFileNumber(rs.getString("filenumber"));
-			    file.setNature(rs.getString("nature"));
-			    file.setRemark(rs.getString("remark"));
-			    file.setSubject(rs.getString("subject"));
-			    fileList.add(file);
+		List<FileListViewDto> fileList = new ArrayList<>();
+
+		Connection con = null;
+
+		try {
+			con = DataAccess.getConnection();
+			CallableStatement prepareCall = con.prepareCall("SELECT * from public.get_file_list(?,?,?,?,?,?)");
+			prepareCall.setLong(1, userPostId);
+			prepareCall.setString(2, keyword);
+			prepareCall.setInt(3, start);
+			prepareCall.setInt(4, end);
+			prepareCall.setString(5, orderBy);
+			prepareCall.setString(6, order);
+			boolean execute = prepareCall.execute();
+
+			if (execute) {
+				ResultSet rs = prepareCall.getResultSet();
+				while (rs.next()) {
+					FileListViewDto file = new FileListViewDto();
+
+					System.out.println("category : " + rs.getString("category") + ", created on : "
+							+ rs.getDate("createdon") + ", doc File : " + rs.getLong("docfileid") + ", File number : "
+							+ rs.getString("filenumber")
+
+					);
+					file.setCategory(rs.getString("category"));
+					file.setCreateDate(rs.getDate("createdon"));
+					file.setDocFileId(rs.getLong("docfileid"));
+					file.setFileNumber(rs.getString("filenumber"));
+					file.setNature(rs.getString("nature"));
+					file.setRemark(rs.getString("remark"));
+					file.setSubject(rs.getString("subject"));
+					fileList.add(file);
+				}
+
 			}
-			
-		}
-	} catch (SQLException e) {
-		e.printStackTrace();
-	}finally {
-		 DataAccess.cleanUp(con);
-		 
-	 }
-	
-	
-	return fileList;
-}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DataAccess.cleanUp(con);
 
-
-@Override
-public int getFileInboxListCount(long userpostId, String keyword) {
-	Connection con=null;
-	int count=0;
-	 try {
-		con = DataAccess.getConnection();
-		CallableStatement prepareCall = con.prepareCall("select get_file_inbox_lists_count(?,?)");
-		prepareCall.setLong(1, userpostId);
-		prepareCall.setString(2, keyword);
-		boolean execute = prepareCall.execute();
-		
-		if(execute) {
-			ResultSet rs=prepareCall.getResultSet();
-			if (rs.next()) {
-			    count= rs.getInt(1);
-			}
 		}
-	} catch (SQLException e) {
-		e.printStackTrace();
+
+		return fileList;
 	}
-	 finally {
-		 DataAccess.cleanUp(con);
-	 }
-	 
-	 
-	
-	return count;
-}
 
+	@Override
+	public int getFileInboxListCount(long userpostId, String keyword) {
+		Connection con = null;
+		int count = 0;
+		try {
+			con = DataAccess.getConnection();
+			CallableStatement prepareCall = con.prepareCall("select get_file_inbox_lists_count(?,?)");
+			prepareCall.setLong(1, userpostId);
+			prepareCall.setString(2, keyword);
+			boolean execute = prepareCall.execute();
 
-@Override
-public List<FileMovementDTO> getFileInboxList(long userPostId, String keyword, int start, int end, String orderBy,
-		String order) {
-	Connection con=null;
-	
-	 try {
-		con = DataAccess.getConnection();
-		CallableStatement prepareCall = con.prepareCall("SELECT * from public.get_file_list(?,?,?,?,?,?)");
-		prepareCall.setLong(1, userPostId);
-		prepareCall.setString(2, keyword);
-		prepareCall.setInt(3, start);
-		prepareCall.setInt(4, end);
-		prepareCall.setString(5, orderBy);
-		prepareCall.setString(6, order);
-		boolean execute = prepareCall.execute();
-		
-		if(execute) {
-			ResultSet rs=prepareCall.getResultSet();
-			while (rs.next()) {
-				FileMovementDTO fileInbox=new FileMovementDTO();
-				
-			
-				
-				
+			if (execute) {
+				ResultSet rs = prepareCall.getResultSet();
+				if (rs.next()) {
+					count = rs.getInt(1);
+				}
 			}
-			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DataAccess.cleanUp(con);
 		}
-	} catch (SQLException e) {
-		e.printStackTrace();
-	}finally {
-		 DataAccess.cleanUp(con);
-		 
-	 }
-	
-	return null;
-}
 
-
-@Override
-public int getFileSentListCount(long postId, String keyword) {
-	Connection con=null;
-	int count=0;
-	 try {
-		con = DataAccess.getConnection();
-		CallableStatement prepareCall = con.prepareCall("select get_file_sent_lists_count(?,?)");
-		prepareCall.setLong(1, postId);
-		prepareCall.setString(2, keyword);
-		boolean execute = prepareCall.execute();
-		
-		if(execute) {
-			ResultSet rs=prepareCall.getResultSet();
-			if (rs.next()) {
-			    count= rs.getInt(1);
-			}
-		}
-	} catch (SQLException e) {
-		e.printStackTrace();
+		return count;
 	}
-	 finally {
-		 DataAccess.cleanUp(con);
-	 }
-	return count;
-	 
-}
 
+	@Override
+	public List<FileMovementDTO> getFileInboxList(long userPostId, String keyword, int start, int end, String orderBy,
+			String order) {
 
-@Override
-public List<FileMovementDTO> getFileSentList(long userPostId, String keyword, int start, int end, String orderBy,
-		String order) {
-	// TODO Auto-generated method stub
-	return null;
-}
+		List<FileMovementDTO> fileMovementDTOList = new ArrayList<>();
+
+		Connection con = null;
+
+		try {
+			con = DataAccess.getConnection();
+			CallableStatement prepareCall = con.prepareCall("SELECT * from public.get_file_list(?,?,?,?,?,?)");
+			prepareCall.setLong(1, userPostId);
+			prepareCall.setString(2, keyword);
+			prepareCall.setInt(3, start);
+			prepareCall.setInt(4, end);
+			prepareCall.setString(5, orderBy);
+			prepareCall.setString(6, order);
+			boolean execute = prepareCall.execute();
+
+			if (execute) {
+				ResultSet rs = prepareCall.getResultSet();
+				while (rs.next()) {
+					FileMovementDTO fileMovementDTO = new FileMovementDTO();
+
+					fileMovementDTO.setFileMovementId(rs.getLong("filemovementid"));
+					fileMovementDTO.setFileNumber(rs.getString("filenumber"));
+					fileMovementDTO.setSubject(rs.getString("subject"));
+					fileMovementDTO.setSentBy(rs.getString("sentby"));
+					fileMovementDTO.setSentTo(rs.getString("sentto"));
+					fileMovementDTO.setSentOn(rs.getDate("senton"));
+					fileMovementDTO.setReadOn(rs.getString("readon"));
+					fileMovementDTO.setDueDate(rs.getString("duedate"));
+					fileMovementDTO.setRemark(rs.getString("remark"));
+					fileMovementDTO.setReceivedOn(rs.getString("receivedon"));
+					fileMovementDTO.setCurrentlyWith(rs.getLong("currentlywith"));
+					fileMovementDTO.setNature(rs.getString("nature"));
+					fileMovementDTO.setFileId(rs.getLong("fileid"));
+					fileMovementDTO.setSenderId(rs.getLong("senderid"));
+					fileMovementDTO.setCurrentState(rs.getInt("currentstate"));
+					fileMovementDTO.setDocFileId(rs.getLong("docfileid"));
+					fileMovementDTO.setPullBackRemark(rs.getString("pullbackrremark"));
+
+					fileMovementDTOList.add(fileMovementDTO);
+				}
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DataAccess.cleanUp(con);
+
+		}
+
+		return fileMovementDTOList;
+	}
+
+	@Override
+	public int getFileSentListCount(long postId, String keyword) {
+		Connection con = null;
+		int count = 0;
+		try {
+			con = DataAccess.getConnection();
+			CallableStatement prepareCall = con.prepareCall("select get_file_sent_lists_count(?,?)");
+			prepareCall.setLong(1, postId);
+			prepareCall.setString(2, keyword);
+			boolean execute = prepareCall.execute();
+
+			if (execute) {
+				ResultSet rs = prepareCall.getResultSet();
+				if (rs.next()) {
+					count = rs.getInt(1);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DataAccess.cleanUp(con);
+		}
+		return count;
+
+	}
+
+	@Override
+	public List<FileMovementDTO> getFileSentList(long userPostId, String keyword, int start, int end, String orderBy,
+			String order) {
+		List<FileMovementDTO> fileMovementDTOList = new ArrayList<>();
+
+		Connection con = null;
+
+		try {
+			con = DataAccess.getConnection();
+			CallableStatement prepareCall = con.prepareCall("SELECT * from public.get_file_list(?,?,?,?,?,?)");
+			prepareCall.setLong(1, userPostId);
+			prepareCall.setString(2, keyword);
+			prepareCall.setInt(3, start);
+			prepareCall.setInt(4, end);
+			prepareCall.setString(5, orderBy);
+			prepareCall.setString(6, order);
+			boolean execute = prepareCall.execute();
+
+			if (execute) {
+				ResultSet rs = prepareCall.getResultSet();
+				while (rs.next()) {
+					FileMovementDTO fileMovementDTO = new FileMovementDTO();
+
+					fileMovementDTO.setFileMovementId(rs.getLong("filemovementid"));
+					fileMovementDTO.setFileNumber(rs.getString("filenumber"));
+					fileMovementDTO.setSubject(rs.getString("subject"));
+					fileMovementDTO.setSentBy(rs.getString("sentby"));
+					fileMovementDTO.setSentTo(rs.getString("sentto"));
+					fileMovementDTO.setSentOn(rs.getDate("senton"));
+					fileMovementDTO.setReadOn(rs.getString("readon"));
+					fileMovementDTO.setDueDate(rs.getString("duedate"));
+					fileMovementDTO.setRemark(rs.getString("remark"));
+					fileMovementDTO.setReceivedOn(rs.getString("receivedon"));
+					fileMovementDTO.setCurrentlyWith(rs.getLong("currentlywith"));
+					fileMovementDTO.setNature(rs.getString("nature"));
+					fileMovementDTO.setFileId(rs.getLong("fileid"));
+					fileMovementDTO.setSenderId(rs.getLong("senderid"));
+					fileMovementDTO.setCurrentState(rs.getInt("currentstate"));
+					fileMovementDTO.setDocFileId(rs.getLong("docfileid"));
+					fileMovementDTO.setPullBackRemark(rs.getString("pullbackrremark"));
+
+					fileMovementDTOList.add(fileMovementDTO);
+				}
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DataAccess.cleanUp(con);
+
+		}
+
+		return fileMovementDTOList;
+
+	}
 
 }
--- modules/jet-process/jet-process-list/src/main/java/io/jetprocess/list/service/ReceiptListImpl.java
+++ modules/jet-process/jet-process-list/src/main/java/io/jetprocess/list/service/ReceiptListImpl.java
@@ -16,56 +16,48 @@ import io.jetprocess.masterdata.model.FileListViewDto;
 import io.jetprocess.masterdata.model.ReceiptListViewDto;
 import io.jetprocess.masterdata.model.ReceiptMovementDTO;
 
-@Component(
-		immediate = true,
-		service=ReceiptList.class
-		)
+@Component(immediate = true, service = ReceiptList.class)
 public class ReceiptListImpl implements ReceiptList {
 
 	@Override
 	public int getReceiptListCount(long userpostId, String keyword) {
-		
-		Connection con=null;
-		int count=0;
-		 try {
+
+		Connection con = null;
+		int count = 0;
+		try {
 			con = DataAccess.getConnection();
-			CallableStatement prepareCall = con.prepareCall("-------paste the function----------");
+			CallableStatement prepareCall = con.prepareCall("select * from public.get_receipt_created_list_count(?,?)" );
 			prepareCall.setLong(1, userpostId);
 			prepareCall.setString(2, keyword);
 			boolean execute = prepareCall.execute();
-			
-			if(execute) {
-				ResultSet rs=prepareCall.getResultSet();
+
+			if (execute) {
+				ResultSet rs = prepareCall.getResultSet();
 				if (rs.next()) {
-				    count= rs.getInt(1);
+					count = rs.getInt(1);
 				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			DataAccess.cleanUp(con);
 		}
-		 finally {
-			 DataAccess.cleanUp(con);
-		 }
-		 
-		 
-		
+
 		return count;
-		
-		
-		
-		
+
 	}
 
 	@Override
 	public List<ReceiptListViewDto> getReceiptList(long userPostId, String keyword, int start, int end, String orderBy,
 			String order) {
+
 		List<ReceiptListViewDto> receiptList = new ArrayList<>();
-		
-		Connection con=null;
-		
-		 try {
+
+		Connection con = null;
+
+		try {
 			con = DataAccess.getConnection();
-			CallableStatement prepareCall = con.prepareCall("-----paste function----------------");
+			CallableStatement prepareCall = con.prepareCall("select * from public.get_receipt_created_list(?,?,?,?,?,?)");
 			prepareCall.setLong(1, userPostId);
 			prepareCall.setString(2, keyword);
 			prepareCall.setInt(3, start);
@@ -73,96 +65,222 @@ public class ReceiptListImpl implements ReceiptList {
 			prepareCall.setString(5, orderBy);
 			prepareCall.setString(6, order);
 			boolean execute = prepareCall.execute();
-			
-			if(execute) {
-				ResultSet rs=prepareCall.getResultSet();
+
+			if (execute) {
+				ResultSet rs = prepareCall.getResultSet();
 				while (rs.next()) {
 					ReceiptListViewDto receipt = new ReceiptListViewDto();
-					
-					System.out.println("category : "+rs.getString("category")+
-							", created on : "+rs.getDate("createdon")+", doc File : "+
-							rs.getLong("docfileid")+", File number : "+rs.getString("filenumber")
-									
-									);
+
+					System.out.println("category : " + rs.getString("category") + ", created on : "
+							+ rs.getDate("createdon") + ", receiptId : " + rs.getLong("receiptId") + ", receiptnumber : "
+							+ rs.getString("receiptnumber")
+
+					);
 					receipt.setReceiptId(rs.getLong("receiptId"));
-					receipt.setReceiptNumber(rs.getString("category"));
+					receipt.setReceiptNumber(rs.getString("receiptnumber"));
 					receipt.setSubject(rs.getString("subject"));
 					receipt.setCategory(rs.getString("category"));
-					receipt.setCreateDate(rs.getDate("createdon"));
+					receipt.setCreateDate(rs.getDate("createdate"));
 					receipt.setNature(rs.getString("nature"));
 					receipt.setRemark(rs.getString("remark"));
-					receipt.setViewPdfUrl(rs.getString("remark"));
-				    receiptList.add(receipt);
+					receipt.setViewPdfUrl(rs.getString("viewpdfurl"));
+					receiptList.add(receipt);
 				}
-				
+
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally {
-			 DataAccess.cleanUp(con);
-			 
-		 }
-		
-		
+		} finally {
+			DataAccess.cleanUp(con);
+
+		}
+
 		return receiptList;
 
 	}
 
 	@Override
 	public int getReceiptInboxListCount(long userpostId, String keyword) {
-		
-		Connection con=null;
-		int count=0;
-		 try {
+
+		Connection con = null;
+		int count = 0;
+		try {
 			con = DataAccess.getConnection();
-			CallableStatement prepareCall = con.prepareCall("-------paste the function----------");
+			CallableStatement prepareCall = con.prepareCall("select * from public.get_receipt_inbox_list_count(?,?)");
 			prepareCall.setLong(1, userpostId);
 			prepareCall.setString(2, keyword);
 			boolean execute = prepareCall.execute();
-			
-			if(execute) {
-				ResultSet rs=prepareCall.getResultSet();
+
+			if (execute) {
+				ResultSet rs = prepareCall.getResultSet();
 				if (rs.next()) {
-				    count= rs.getInt(1);
+					count = rs.getInt(1);
 				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			DataAccess.cleanUp(con);
 		}
-		 finally {
-			 DataAccess.cleanUp(con);
-		 }
-		 
-		 
-		
+
 		return count;
 
-		
-		
-		
-		
-		
 	}
 
 	@Override
 	public List<ReceiptMovementDTO> getReceiptInboxList(long userPostId, String keyword, int start, int end,
 			String orderBy, String order) {
-	
-		return null;
+
+		List<ReceiptMovementDTO> receiptMovementDTOList = new ArrayList<>();
+
+		Connection con = null;
+
+		try {
+			con = DataAccess.getConnection();
+			CallableStatement prepareCall = con.prepareCall("select * from public.get_receipt_inbox_list(?,?,?,?,?.?)");
+			prepareCall.setLong(1, userPostId);
+			prepareCall.setString(2, keyword);
+			prepareCall.setInt(3, start);
+			prepareCall.setInt(4, end);
+			prepareCall.setString(5, orderBy);
+			prepareCall.setString(6, order);
+			boolean execute = prepareCall.execute();
+
+			if (execute) {
+				ResultSet rs = prepareCall.getResultSet();
+				while (rs.next()) {
+					ReceiptMovementDTO receiptMovementDTO = new ReceiptMovementDTO();
+
+					System.out.println("receiptmovementid : " + rs.getString("receiptmovementid") + ", receiptnumber : "
+							+ rs.getString("receiptnumber") + ", subject : " + rs.getString("subject") + ", sender : "
+							+ rs.getString("sender") + ", sentby : " + rs.getString("sentby") + ", sentto : "
+							+ rs.getString("sentto") + ", senton : " + rs.getDate("senton") + ", readon : "
+							+ rs.getString("readon") + ", duedate : " + rs.getString("duedate") + ", remark : "
+							+ rs.getString("remark") + ", receivedon : " + rs.getString("receivedon") + ", nature : "
+							+ rs.getString("nature") + ", receiptid : " + rs.getLong("receiptid") + ", pullbackremark : "
+							+ rs.getString("pullbackremark")
+
+					);
+
+					receiptMovementDTO.setReceiptMovementId(rs.getLong("receiptmovementid"));
+					receiptMovementDTO.setReceiptNumber(rs.getString("receiptnumber"));
+					receiptMovementDTO.setSubject(rs.getString("subject"));
+					receiptMovementDTO.setSender(rs.getString("sender"));
+					receiptMovementDTO.setSentBy(rs.getString("sentby"));
+					receiptMovementDTO.setSentTo(rs.getString("sentto"));
+					receiptMovementDTO.setSentOn(rs.getDate("senton"));
+					receiptMovementDTO.setReadOn(rs.getString("readon"));
+					receiptMovementDTO.setDueDate(rs.getString("duedate"));
+					receiptMovementDTO.setRemark(rs.getString("remark"));
+					receiptMovementDTO.setReceivedOn(rs.getString("receivedon"));
+					receiptMovementDTO.setNature(rs.getString("nature"));
+					receiptMovementDTO.setReceiptId(rs.getLong("receiptid"));
+					receiptMovementDTO.setPullBackRemark(rs.getString("pullbackremark"));
+
+					receiptMovementDTOList.add(receiptMovementDTO);
+				}
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DataAccess.cleanUp(con);
+
+		}
+
+		return receiptMovementDTOList;
+
 	}
 
 	@Override
 	public int getReceiptSentListCount(long userpostId, String keyword) {
-		// TODO Auto-generated method stub
-		return 0;
+
+		Connection con = null;
+		int count = 0;
+		try {
+			con = DataAccess.getConnection();
+			CallableStatement prepareCall = con.prepareCall("select * from public.get_receipt_send_list_count(?,?)");
+			prepareCall.setLong(1, userpostId);
+			prepareCall.setString(2, keyword);
+			boolean execute = prepareCall.execute();
+
+			if (execute) {
+				ResultSet rs = prepareCall.getResultSet();
+				if (rs.next()) {
+					count = rs.getInt(1);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DataAccess.cleanUp(con);
+		}
+
+		return count;
 	}
 
 	@Override
 	public List<ReceiptMovementDTO> getReceiptSentList(long userPostId, String keyword, int start, int end,
 			String orderBy, String order) {
-		// TODO Auto-generated method stub
-		return null;
+
+		List<ReceiptMovementDTO> receiptMovementDTOList = new ArrayList<>();
+
+		Connection con = null;
+
+		try {
+			con = DataAccess.getConnection();
+			CallableStatement prepareCall = con.prepareCall("select * from public.get_receipt_sent_list(?,?,?,?,?,?)");
+			prepareCall.setLong(1, userPostId);
+			prepareCall.setString(2, keyword);
+			prepareCall.setInt(3, start);
+			prepareCall.setInt(4, end);
+			prepareCall.setString(5, orderBy);
+			prepareCall.setString(6, order);
+			boolean execute = prepareCall.execute();
+
+			if (execute) {
+				ResultSet rs = prepareCall.getResultSet();
+				while (rs.next()) {
+					ReceiptMovementDTO receiptMovementDTO = new ReceiptMovementDTO();
+
+					System.out.println("receiptmovementid : " + rs.getString("receiptmovementid") + ", receiptnumber : "
+							+ rs.getString("receiptnumber") + ", subject : " + rs.getString("subject") + ", sender : "
+							+ rs.getString("sender") + ", sentby : " + rs.getString("sentby") + ", sentto : "
+							+ rs.getString("sentto") + ", senton : " + rs.getDate("senton") + ", readon : "
+							+ rs.getString("readon") + ", duedate : " + rs.getString("duedate") + ", remark : "
+							+ rs.getString("remark") + ", receivedon : " + rs.getString("receivedon") + ", nature : "
+							+ rs.getString("nature") + ", receiptid : " + rs.getLong("receiptid") + ", pullbackremark : "
+							+ rs.getString("pullbackremark")
+
+					);
+
+					receiptMovementDTO.setReceiptMovementId(rs.getLong("receiptmovementid"));
+					receiptMovementDTO.setReceiptNumber(rs.getString("receiptnumber"));
+					receiptMovementDTO.setSubject(rs.getString("subject"));
+					receiptMovementDTO.setSender(rs.getString("sender"));
+					receiptMovementDTO.setSentBy(rs.getString("sentby"));
+					receiptMovementDTO.setSentTo(rs.getString("sentto"));
+					receiptMovementDTO.setSentOn(rs.getDate("senton"));
+					receiptMovementDTO.setReadOn(rs.getString("readon"));
+					receiptMovementDTO.setDueDate(rs.getString("duedate"));
+					receiptMovementDTO.setRemark(rs.getString("remark"));
+					receiptMovementDTO.setReceivedOn(rs.getString("receivedon"));
+					receiptMovementDTO.setNature(rs.getString("nature"));
+					receiptMovementDTO.setReceiptId(rs.getLong("receiptid"));
+					receiptMovementDTO.setPullBackRemark(rs.getString("pullbackremark"));
+
+					receiptMovementDTOList.add(receiptMovementDTO);
+				}
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DataAccess.cleanUp(con);
+
+		}
+
+		return receiptMovementDTOList;
+
 	}
-
-
 }
