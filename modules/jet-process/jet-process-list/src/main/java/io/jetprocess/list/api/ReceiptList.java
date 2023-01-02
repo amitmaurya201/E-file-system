dadd3bd8d7707d2094ef6e79b5b36d6a7f9418ad
Amit <amit.maurya@adjecti.com>
2023-01-02 10:46:42 +0530

    fix

--- modules/jet-process/jet-process-list/src/main/java/io/jetprocess/list/api/ReceiptList.java
+++ modules/jet-process/jet-process-list/src/main/java/io/jetprocess/list/api/ReceiptList.java
@@ -22,4 +22,19 @@ public interface ReceiptList {
 	
 	public List<ReceiptMovementDTO> getReceiptSentList(long userPostId, String keyword, int start, int end,
 			String orderBy, String order);
+	
+	
+	public List<ReceiptMovementDTO> getReceiptMovementList(long receiptId, String keyword, int start, int end,
+			String orderBy, String order);
+	
+	public int getReceiptMovementListCount(long userpostId, String keyword);
+	
+	
+	public List<ReceiptListViewDto> getPutInFileList(long userPostId, String keyword, int start, int end,
+			String orderBy, String order);
+	
+	public int getPutInFileListCount(long postId, String keyword);
+			
+	
+	
 }
