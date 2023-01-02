da4f7ae46c91a60dab4fd38872387b5238c8bf7a
liferay12 <ashwani.liferay@gmail.com>
2023-01-02 12:34:59 +0530

    added file created list query

--- modules/jet-process/jet-process-list/src/main/java/io/jetprocess/list/service/FileListImpl.java
+++ modules/jet-process/jet-process-list/src/main/java/io/jetprocess/list/service/FileListImpl.java
@@ -24,7 +24,7 @@ public class FileListImpl implements FileList {
 		int count = 0;
 		try {
 			con = DataAccess.getConnection();
-			CallableStatement prepareCall = con.prepareCall("select file_count_by_keyword(?,?)");
+			CallableStatement prepareCall = con.prepareCall("select public.get_file_created_list_count(?,?)");
 			prepareCall.setLong(1, postId);
 			prepareCall.setString(2, keyword);
 			boolean execute = prepareCall.execute();
