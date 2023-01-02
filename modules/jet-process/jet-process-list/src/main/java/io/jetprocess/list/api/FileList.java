e15c2127f82a8362ef7b82f40aaec19af35498dc
Amit <amit.maurya@adjecti.com>
2023-01-02 14:34:02 +0530

    fix

--- modules/jet-process/jet-process-list/src/main/java/io/jetprocess/list/api/FileList.java
+++ modules/jet-process/jet-process-list/src/main/java/io/jetprocess/list/api/FileList.java
@@ -25,4 +25,11 @@ public interface FileList {
 	public List<FileMovementDTO> getFileSentList(long userPostId, String keyword, int start, int end,
 			String orderBy, String order);
 	
+	
+	public List<FileMovementDTO> getFileMovementList(long docfileId, String keyword, int start, int end,
+			String orderBy, String order);
+	
+	public int getFileMovementListCount(long postId, String keyword);
+	
+	
 }
