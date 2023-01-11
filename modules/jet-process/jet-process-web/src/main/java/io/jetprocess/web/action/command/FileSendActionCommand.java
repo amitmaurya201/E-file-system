80858be0eb3be14fb3331bc99a8dfd30fd88a9d4
Amit <amit.maurya@adjecti.com>
2023-01-11 14:21:21 +0530

    new action command added for sendfile , send receipt

---
+++ modules/jet-process/jet-process-web/src/main/java/io/jetprocess/web/action/command/FileSendActionCommand.java
@@ -0,0 +1,17 @@
+package io.jetprocess.web.action.command;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+
+public class FileSendActionCommand  implements MVCActionCommand{
+
+	@Override
+	public boolean processAction(ActionRequest actionRequest, ActionResponse actionResponse) throws PortletException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
