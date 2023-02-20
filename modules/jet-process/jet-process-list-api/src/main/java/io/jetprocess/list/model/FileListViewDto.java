5a238b4faee2747ba64b96312549e485af60d982
Amit <amit.maurya@adjecti.com>
2023-02-20 11:25:33 +0530

    fix

--- modules/jet-process/jet-process-list-api/src/main/java/io/jetprocess/list/model/FileListViewDto.java
+++ modules/jet-process/jet-process-list-api/src/main/java/io/jetprocess/list/model/FileListViewDto.java
@@ -12,73 +12,63 @@ public class FileListViewDto {
 	private String remark;
 	private Date createDate;
 	private String nature;
-
+	private boolean isRead;
 	public long getFilemovementId() {
 		return filemovementId;
 	}
-
 	public void setFilemovementId(long filemovementId) {
 		this.filemovementId = filemovementId;
 	}
-
 	public long getDocFileId() {
 		return docFileId;
 	}
-
 	public void setDocFileId(long docFileId) {
 		this.docFileId = docFileId;
 	}
-
 	public String getFileNumber() {
 		return fileNumber;
 	}
-
 	public void setFileNumber(String fileNumber) {
 		this.fileNumber = fileNumber;
 	}
-
 	public String getSubject() {
 		return subject;
 	}
-
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
-
 	public String getCategory() {
 		return category;
 	}
-
 	public void setCategory(String category) {
 		this.category = category;
 	}
-
 	public String getRemark() {
 		return remark;
 	}
-
 	public void setRemark(String remark) {
 		this.remark = remark;
 	}
-
 	public Date getCreateDate() {
 		return createDate;
 	}
-
 	public void setCreateDate(Date createDate) {
 		this.createDate = createDate;
 	}
-
 	public String getNature() {
 		return nature;
 	}
-
 	public void setNature(String nature) {
 		this.nature = nature;
 	}
-
+	public boolean isRead() {
+		return isRead;
+	}
+	public void setRead(boolean isRead) {
+		this.isRead = isRead;
+	}
 	public FileListViewDto(long filemovementId, long docFileId, String fileNumber, String subject, String category,
-			String remark, Date createDate, String nature) {
+			String remark, Date createDate, String nature, boolean isRead) {
 		super();
 		this.filemovementId = filemovementId;
 		this.docFileId = docFileId;
@@ -88,11 +78,12 @@ public class FileListViewDto {
 		this.remark = remark;
 		this.createDate = createDate;
 		this.nature = nature;
+		this.isRead = isRead;
 	}
-
 	public FileListViewDto() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	
 }
