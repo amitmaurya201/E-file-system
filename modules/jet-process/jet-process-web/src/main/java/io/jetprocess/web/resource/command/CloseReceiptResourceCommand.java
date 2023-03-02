125e3234509e71e71d9aac70190dd174b6d36f60
Deepanshu verma <deepanshu.verma@adjecti.com>
2023-03-02 12:15:11 +0530

    fix

--- modules/jet-process/jet-process-web/src/main/java/io/jetprocess/web/resource/command/CloseReceiptResourceCommand.java
+++ modules/jet-process/jet-process-web/src/main/java/io/jetprocess/web/resource/command/CloseReceiptResourceCommand.java
@@ -11,38 +11,37 @@ import javax.portlet.ResourceResponse;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
+import io.jetprocess.service.ReceiptCloseDetailLocalService;
 //import io.jetprocess.service.ReceiptCloseDetailLocalService;
 import io.jetprocess.web.constants.JetProcessWebPortletKeys;
 import io.jetprocess.web.constants.MVCCommandNames;
 
 @Component(property = { "javax.portlet.name=" + JetProcessWebPortletKeys.JETPROCESSWEB,
-		"mvc.command.name="+MVCCommandNames.CLOSE_RECEIPT_RESOURCE_COMMAND }, service = MVCResourceCommand.class)
+		"mvc.command.name=" + MVCCommandNames.CLOSE_RECEIPT_RESOURCE_COMMAND }, service = MVCResourceCommand.class)
 
-public class CloseReceiptResourceCommand implements MVCResourceCommand  {
+public class CloseReceiptResourceCommand implements MVCResourceCommand {
 
 	@Override
 	public boolean serveResource(ResourceRequest resourceRequest, ResourceResponse resourceResponse)
 			throws PortletException {
-		
-		
-		long closedMovementId =	ParamUtil.getLong(resourceRequest, "rmId");
-		
-		long receiptId = ParamUtil.getLong(resourceRequest,"receiptId");
-		String closingRemarks = ParamUtil.getString(resourceRequest,"closingRemarks");
+
+		long closedMovementId = ParamUtil.getLong(resourceRequest, "rmId");
+		long receiptId = ParamUtil.getLong(resourceRequest, "receiptId");
+		String closingRemarks = ParamUtil.getString(resourceRequest, "closingRemarks");
 		String userPostId = ParamUtil.getString(resourceRequest, "userPostId");
-	    long closedBy = Long.parseLong(userPostId);
-		/*
-		 * try { //receiptCloseDetailLocalService.addClosedReceiptDetails(receiptId,
-		 * closedBy, closingRemarks, closedMovementId); } catch (PortalException e) {
-		 * e.printStackTrace(); }
-		 */
+		long closedBy = Long.parseLong(userPostId);
+
+		try {
+			receiptCloseDetailLocalService.addClosedReceiptDetails(receiptId, closedBy, closingRemarks,
+					closedMovementId);
+		} catch (PortalException e) {
+			e.printStackTrace();
+		}
+
 		return false;
 	}
-	
-	
-	/*
-	 * @Reference private ReceiptCloseDetailLocalService
-	 * receiptCloseDetailLocalService;
-	 */
+
+	@Reference
+	private ReceiptCloseDetailLocalService receiptCloseDetailLocalService;
 
 }
