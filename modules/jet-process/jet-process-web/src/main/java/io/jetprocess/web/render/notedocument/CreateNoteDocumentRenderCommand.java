83d8ec76ce73f74f9688b94c80646fbda8dcbf49
liferay12 <ashwani.liferay@gmail.com>
2023-03-12 22:45:16 +0530

    fixed

--- modules/jet-process/jet-process-web/src/main/java/io/jetprocess/web/render/notedocument/CreateNoteDocumentRenderCommand.java
+++ modules/jet-process/jet-process-web/src/main/java/io/jetprocess/web/render/notedocument/CreateNoteDocumentRenderCommand.java
@@ -4,12 +4,17 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
 
+import java.util.List;
+
 import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
+import io.jetprocess.masterdata.model.Masterdata;
+import io.jetprocess.masterdata.service.MasterdataLocalService;
 import io.jetprocess.web.constants.JetProcessWebPortletKeys;
 import io.jetprocess.web.constants.MVCCommandNames;
 
@@ -20,9 +25,13 @@ public class CreateNoteDocumentRenderCommand  implements MVCRenderCommand{
 	@Override
 	public String render(RenderRequest renderRequest, RenderResponse renderResponse) throws PortletException {
 		logger.info("CreateNoteDocumentRenderCommand------>");
+		List<Masterdata> subjectCategoryList = masterdataLocalService.getSubjectCategory();
+		renderRequest.setAttribute("subjectCategoryList", subjectCategoryList);
 		return "/note-document/create-note-document.jsp";
 	}
 	private Log logger = LogFactoryUtil.getLog(CreateNoteDocumentRenderCommand.class);
 
+	@Reference
+	private MasterdataLocalService masterdataLocalService;
 
 }
